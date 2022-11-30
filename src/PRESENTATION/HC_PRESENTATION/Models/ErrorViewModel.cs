9a70f97a43243189b133cb3ca797732266d93438
Bahadir <Bahadir@ARDA>
2022-12-01 01:15:44 +0300

    Add project files.

---
+++ src/PRESENTATION/HC_PRESENTATION/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HC_PRESENTATION.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
