111e92497a6495bb62a1aa073947868ba6ff8ea7
ErkutKamis <erkutkamis@gmail.com>
2022-12-03 00:10:24 +0300

    AppUser created.

---
+++ src/DOMAIN/HC_DOMAIN/Entities/Concrete/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_DOMAIN.Entities.Concrete
+{
+    public class AppUser
+    {
+        public string FullName { get; set; }
+       
+    }
+}
