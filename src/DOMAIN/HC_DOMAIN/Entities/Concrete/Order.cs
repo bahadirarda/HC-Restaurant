039a2f5d4be4a49eaae567460e8813e01d6667e8
Bahadir Arda <bahadirarda96@gmail.com>
2022-12-04 06:35:03 +0300

    Concrete done.

--- src/DOMAIN/HC_DOMAIN/Entities/Concrete/Order.cs
+++ src/DOMAIN/HC_DOMAIN/Entities/Concrete/Order.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HC_DOMAIN.Entities.Interface;
+using HC_DOMAIN.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace HC_DOMAIN.Entities.Concrete
 {
-    internal class Order
+    public class Order: IBaseEntity
     {
+        public Order()
+        {
+            Status = Status.Active;
+        }
+        public Guid ID { get; set; }
+
+        public Status Status { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatorUserName { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdaterUserName { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public string DeleterUserName { get; set; }
+
+        //Relations
+        public Guid EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        public string AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+        public virtual List<OrderDetail> OrderDetails { get; set; }
     }
 }
---
+++ src/DOMAIN/HC_DOMAIN/Entities/Interface/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_DOMAIN.Entities.Interface
+{
+    public interface IEntity<TEntity>
+    {
+        TEntity ID { get; set; } // Full control on ID
+    }
+}
