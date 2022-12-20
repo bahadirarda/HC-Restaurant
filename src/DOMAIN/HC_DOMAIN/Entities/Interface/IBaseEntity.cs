4172793165da47546a8106fe2bd71c8ff60b1f49
Bahadır Arda <bahadirarda96@gmail.com>
2022-12-20 17:37:04 +0300

    IBaseEntity changed

--- src/DOMAIN/HC_DOMAIN/Entities/Interface/IBaseEntity.cs
+++ src/DOMAIN/HC_DOMAIN/Entities/Interface/IBaseEntity.cs
@@ -13,15 +13,12 @@ namespace HC_DOMAIN.Entities.Interface
         // When data added.
         public DateTime? CreatedDate { get; set; }
         public string? CreatedBy { get; set; }
-        public string CreatorUserName { get; set; }
         // When data updated.
         public DateTime? UpdatedDate { get; set; }
         public string? UpdatedBy { get; set; }
-        public string UpdaterUserName { get; set; }
         // When data deleted.
         public DateTime? DeletedDate { get; set; }
         public string? DeletedBy { get; set; }
-        public string DeleterUserName { get; set; }
         
     }
 }
